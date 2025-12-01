ea545a6e3ee145d12397342761816e8f9c8131c7
Dima457 <baklagovdima8@gmail.com>
2025-12-01 11:35:16 +0300

    change ./src/main.tsx

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,9 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: false, // Отключаем для продакшена
     rollupOptions: {
+      input: {
+        main: './index.html' // Из первой конфигурации
+      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
